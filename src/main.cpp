5ff63344f35fe14e5dc6a05b4ce96965e18aafec
Carlos A. Moncada <cmoncada@users.noreply.github.com>
2024-08-25 15:56:12 -0500

    upload changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,174 +1,122 @@
 #include <Arduino.h>
 #include <lvgl.h>
 #include <TFT_eSPI.h>
-#include <XPT2046_Touchscreen.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <SPI.h>
 
-// Touchscreen pins
-#define XPT2046_IRQ 36   // T_IRQ
-#define XPT2046_MOSI 32  // T_DIN
-#define XPT2046_MISO 39  // T_OUT
-#define XPT2046_CLK 25   // T_CLK
-#define XPT2046_CS 33    // T_CS
+// Define DHT GPIO PORT
+#define DHTPIN 27
 
-SPIClass touchscreenSPI = SPIClass(VSPI);
-XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+// Define DH Sensor Model
+#define DHTTYPE DHT22
+
+// Setup sensor configuration
+DHT dht(DHTPIN, DHTTYPE);
+
+#define MIN_TEMPERATURE 20
+#define MAX_TEMPERATURE 50
 
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
-
-// Touchscreen coordinates: (x, y) and pressure (z)
-int x, y, z;
 
 #define DRAW_BUF_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT / 10 * (LV_COLOR_DEPTH / 8))
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
 // If logging is enabled, it will inform the user about what is happening in the library
-void log_print(lv_log_level_t level, const char * buf) {
+void log_print(lv_log_level_t level, const char *buf)
+{
   LV_UNUSED(level);
   Serial.println(buf);
   Serial.flush();
 }
 
-// Get the Touchscreen data
-void touchscreen_read(lv_indev_t * indev, lv_indev_data_t * data) {
-  // Checks if Touchscreen was touched, and prints X, Y and Pressure (Z)
-  if(touchscreen.tirqTouched() && touchscreen.touched()) {
-    // Get Touchscreen points
-    TS_Point p = touchscreen.getPoint();
-    // Calibrate Touchscreen points with map function to the correct width and height
-    x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
-    y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
-    z = p.z;
+lv_obj_t *arc;
 
-    data->state = LV_INDEV_STATE_PRESSED;
-
-    // Set the coordinates
-    data->point.x = x;
-    data->point.y = y;
-
-    // Print Touchscreen info about X, Y and Pressure (Z) on the Serial Monitor
-    /* Serial.print("X = ");
-    Serial.print(x);
-    Serial.print(" | Y = ");
-    Serial.print(y);
-    Serial.print(" | Pressure = ");
-    Serial.print(z);
-    Serial.println();*/
+// Set the temperature value in the arc and text label
+static void set_temp(void *text_label_temp_value, int32_t v)
+{
+  float temperature = dht.readTemperature();
+  if (temperature <= 10.0)
+  {
+    lv_obj_set_style_text_color((lv_obj_t *)text_label_temp_value, lv_palette_main(LV_PALETTE_BLUE), 0);
   }
-  else {
-    data->state = LV_INDEV_STATE_RELEASED;
+  else if (temperature > 10.0 && temperature <= 29)
+  {
+    lv_obj_set_style_text_color((lv_obj_t *)text_label_temp_value, lv_palette_main(LV_PALETTE_GREEN), 0);
   }
-}
-
-int btn1_count = 0;
-// Callback that is triggered when btn1 is clicked
-static void event_handler_btn1(lv_event_t * e) {
-  lv_event_code_t code = lv_event_get_code(e);
-  if(code == LV_EVENT_CLICKED) {
-    btn1_count++;
-    LV_LOG_USER("Button clicked %d", (int)btn1_count);
+  else
+  {
+    lv_obj_set_style_text_color((lv_obj_t *)text_label_temp_value, lv_palette_main(LV_PALETTE_RED), 0);
   }
+  const char degree_symbol[] = "\u00B0C";
+
+  lv_arc_set_value(arc, map(int(temperature), MIN_TEMPERATURE, MAX_TEMPERATURE, 0, 100));
+
+  String temperature_text = String(temperature) + degree_symbol;
+  lv_label_set_text((lv_obj_t *)text_label_temp_value, temperature_text.c_str());
+  Serial.print("Temperature: ");
+  Serial.println(temperature_text);
 }
 
-// Callback that is triggered when btn2 is clicked/toggled
-static void event_handler_btn2(lv_event_t * e) {
-  lv_event_code_t code = lv_event_get_code(e);
-  lv_obj_t * obj = (lv_obj_t*) lv_event_get_target(e);
-  if(code == LV_EVENT_VALUE_CHANGED) {
-    LV_UNUSED(obj);
-    LV_LOG_USER("Toggled %s", lv_obj_has_state(obj, LV_STATE_CHECKED) ? "on" : "off");
-  }
+void lv_create_main_gui(void)
+{
+  // Create an Arc
+  arc = lv_arc_create(lv_screen_active());
+  lv_obj_set_size(arc, 210, 210);
+  lv_arc_set_rotation(arc, 135);
+  lv_arc_set_bg_angles(arc, 0, 270);
+  lv_obj_set_style_arc_color(arc, lv_color_hex(0x666666), LV_PART_INDICATOR);
+  lv_obj_set_style_bg_color(arc, lv_color_hex(0x333333), LV_PART_KNOB);
+  lv_obj_align(arc, LV_ALIGN_CENTER, 0, 10);
+
+  // Create a text label in font size 32 to display the latest temperature reading
+  lv_obj_t *text_label_temp_value = lv_label_create(lv_screen_active());
+  lv_label_set_text(text_label_temp_value, "--.--");
+  lv_obj_set_style_text_align(text_label_temp_value, LV_TEXT_ALIGN_CENTER, 0);
+  lv_obj_align(text_label_temp_value, LV_ALIGN_CENTER, 0, 10);
+  static lv_style_t style_temp;
+  lv_style_init(&style_temp);
+  lv_style_set_text_font(&style_temp, &lv_font_montserrat_32);
+  lv_obj_add_style(text_label_temp_value, &style_temp, 0);
+
+  // Create an animation to update with the latest temperature value every 10 seconds
+  lv_anim_t a_temp;
+  lv_anim_init(&a_temp);
+  lv_anim_set_exec_cb(&a_temp, set_temp);
+  lv_anim_set_duration(&a_temp, 100);
+  lv_anim_set_playback_duration(&a_temp, 100);
+  lv_anim_set_var(&a_temp, text_label_temp_value);
+  lv_anim_set_values(&a_temp, 0, 100);
+  lv_anim_set_repeat_count(&a_temp, LV_ANIM_REPEAT_INFINITE);
+  lv_anim_start(&a_temp);
 }
 
-static lv_obj_t * slider_label;
-// Callback that prints the current slider value on the TFT display and Serial Monitor for debugging purposes
-static void slider_event_callback(lv_event_t * e) {
-  lv_obj_t * slider = (lv_obj_t*) lv_event_get_target(e);
-  char buf[8];
-  lv_snprintf(buf, sizeof(buf), "%d%%", (int)lv_slider_get_value(slider));
-  lv_label_set_text(slider_label, buf);
-  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-  LV_LOG_USER("Slider changed to %d%%", (int)lv_slider_get_value(slider));
-}
-
-void lv_create_main_gui(void) {
-  // Create a text label aligned center on top ("Hello, world!")
-  lv_obj_t * text_label = lv_label_create(lv_screen_active());
-  lv_label_set_long_mode(text_label, LV_LABEL_LONG_WRAP);    // Breaks the long lines
-  lv_label_set_text(text_label, "Hello, world!");
-  lv_obj_set_width(text_label, 150);    // Set smaller width to make the lines wrap
-  lv_obj_set_style_text_align(text_label, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_align(text_label, LV_ALIGN_CENTER, 0, -90);
-
-  lv_obj_t * btn_label;
-  // Create a Button (btn1)
-  lv_obj_t * btn1 = lv_button_create(lv_screen_active());
-  lv_obj_add_event_cb(btn1, event_handler_btn1, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -50);
-  lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
-
-  btn_label = lv_label_create(btn1);
-  lv_label_set_text(btn_label, "Button");
-  lv_obj_center(btn_label);
-
-  // Create a Toggle button (btn2)
-  lv_obj_t * btn2 = lv_button_create(lv_screen_active());
-  lv_obj_add_event_cb(btn2, event_handler_btn2, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 10);
-  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-  btn_label = lv_label_create(btn2);
-  lv_label_set_text(btn_label, "Toggle");
-  lv_obj_center(btn_label);
-  
-  // Create a slider aligned in the center bottom of the TFT display
-  lv_obj_t * slider = lv_slider_create(lv_screen_active());
-  lv_obj_align(slider, LV_ALIGN_CENTER, 0, 60);
-  lv_obj_add_event_cb(slider, slider_event_callback, LV_EVENT_VALUE_CHANGED, NULL);
-  lv_slider_set_range(slider, 0, 100);
-  lv_obj_set_style_anim_duration(slider, 2000, 0);
-
-  // Create a label below the slider to display the current slider value
-  slider_label = lv_label_create(lv_screen_active());
-  lv_label_set_text(slider_label, "0%");
-  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-}
-
-void setup() {
+void setup()
+{
   String LVGL_Arduino = String("LVGL Library Version: ") + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
   Serial.begin(115200);
   Serial.println(LVGL_Arduino);
-  
+
+  dht.begin();
+
   // Start LVGL
   lv_init();
   // Register print function for debugging
   lv_log_register_print_cb(log_print);
 
-  // Start the SPI for the touchscreen and init the touchscreen
-  touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
-  touchscreen.begin(touchscreenSPI);
-  // Set the Touchscreen rotation in landscape mode
-  // Note: in some displays, the touchscreen might be upside down, so you might need to set the rotation to 1: touchscreen.setRotation(1);
-  touchscreen.setRotation(3);
-
   // Create a display object
-  lv_display_t * disp;
+  lv_display_t *disp;
   // Initialize the TFT display using the TFT_eSPI library
   disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf, sizeof(draw_buf));
-  
-  // Initialize an LVGL input device object (Touchscreen)
-  lv_indev_t * indev = lv_indev_create();
-  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
-  // Set the callback function to read Touchscreen input
-  lv_indev_set_read_cb(indev, touchscreen_read);
 
-  // Function to draw the GUI (text, buttons and sliders)
+  // Function to draw the GUI
   lv_create_main_gui();
 }
 
-void loop() {
-  lv_task_handler();  // let the GUI do its work
-  lv_tick_inc(5);     // tell LVGL how much time has passed
-  delay(5);           // let this time pass
+void loop()
+{
+  lv_task_handler(); // let the GUI do its work
+  lv_tick_inc(1);    // tell LVGL how much time has passed
+  delay(1);          // let this time pass
 }
